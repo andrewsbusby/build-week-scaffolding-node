7b50a13221ec2bf9724cfda3c687cd2771220870
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-23 16:21:27 -0600

    update to table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -6,6 +6,20 @@ exports.up = async (knex) => {
       users.string('password', 200).notNullable()
       users.timestamps(false, true)
     })
+    .createTable('plants', plants => {
+      plants.increments('plant_id')
+      plants.string('name', 128).notNullable()
+      plants.string('species', 128).notNullable()
+      plants.text('watering_frequency').notNullable
+      plants.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('user_id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+
+    })
 }
 
 exports.down = async (knex) => {
