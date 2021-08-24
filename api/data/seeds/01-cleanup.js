9432f3692d6c10018d674f278a73098fd07e9e47
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-24 12:13:40 -0600

    wip

--- api/data/seeds/01-cleanup.js
+++ api/data/seeds/01-cleanup.js
@@ -1,8 +1,10 @@
 const { clean } = require('knex-cleaner')
 
-exports.seed = function (knex) {
+exports.seeds = function (knex) {
   return clean(knex, {
     mode: 'truncate',
     ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
   })
 }
+
+
